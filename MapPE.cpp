709f1157cfeca03f098648c58b4cf200021d4333
Ege Balcı <egebal1996@gmail.com>
2017-04-26 23:07:21 +0300

    - Fixed errors

--- MapPE.cpp
+++ MapPE.cpp
@@ -5,6 +5,10 @@
 
 using namespace std;
 
+
+void PrintInfo(char *);
+void Dump(char *);
+
 int main(int argc, char const *argv[])
 {
 
@@ -13,8 +17,11 @@ int main(int argc, char const *argv[])
 	File.open (argv[0], std::fstream::in | std::fstream::out | std::fstream::binary);
 	if(File.is_open()){
 
-		PrintInfo();
-		Dump();
+		LPVOID PE = VirtualAlloc(NULL,sizeof(File),MEM_COMMIT,PAGE_READWRITE);
+		memcpy(PE,File,sizeof(File));
+
+		PrintInfo((char*)PE);
+		Dump((char*)PE);
 	}
 	else{
 		cout << "[-] Unable to open file (" << argv[0] << ")\n";
@@ -35,21 +42,20 @@ void PrintInfo(char * PE){
 	_IMAGE_DATA_DIRECTORY * ImportTable;
 	_IMAGE_DATA_DIRECTORY * ImportAddressTable;
 	_IMAGE_DATA_DIRECTORY * ExportTable;
-	IMAGE_SECTION_HEADER * SectionHeader;	
 
 
 	DOSHeader = PIMAGE_DOS_HEADER(PE); // Initialize Variable
 	NtHeader = PIMAGE_NT_HEADERS(DWORD(PE) + DOSHeader->e_lfanew); // Initialize	
-	FileHeader = &NtHeader.FileHeader;
-	OptHeader = &NtHeader.OptionalHeader;
+	FileHeader = &NtHeader->FileHeader;
+	OptHeader = &NtHeader->OptionalHeader;
 
 
 	if(PE[0] == 'M' && PE[0] == 'Z'){
 		cout << "[+] \"MZ\" magic number found !\n";
-		if(NtHeader.Signature == IMAGE_NT_SIGNATURE){
+		if(NtHeader->Signature == IMAGE_NT_SIGNATURE){
 			cout << "[+] Valid \"PE\" signature \n\n";
 
-			cout << "[-------------------------------------]"
+			cout << "[-------------------------------------]\n";
 
 			printf("[*] ImageBase: 0x%x\n", OptHeader->ImageBase);
 			printf("[*] Address Of Entry: 0x%x\n", (OptHeader->ImageBase+OptHeader->AddressOfEntryPoint));
@@ -57,34 +63,34 @@ void PrintInfo(char * PE){
 			cout << "[*] Number Of Sections: " << FileHeader->NumberOfSections << endl;
 			cout << "[*] Number Of Symbols: " << FileHeader->NumberOfSymbols << endl;
 
-			cout << "[*] Size Of Image: " << OptHeader.SizeOfImage << " bytes\n";
-			cout << "[*] Size Of Headers: " << OptHeader.SizeOfHeaders << " bytes\n";
+			cout << "[*] Size Of Image: " << OptHeader->SizeOfImage << " bytes\n";
+			cout << "[*] Size Of Headers: " << OptHeader->SizeOfHeaders << " bytes\n";
 
-			printf("[*] Checksum: 0x%x\n", OptHeader.Checksum)
-
-
-			ExportTable = OptHeader.DataDirectory[0];
-			ImportTable = OptHeader.DataDirectory[1];
-			ImportAddressTable = OptHeader.DataDirectory[12];
+			printf("[*] Checksum: 0x%x\n", OptHeader->CheckSum);
 
 
-			printf("[*] Export Table: 0x%x\n", (ExportTable.VirtualAddress+OptHeader.ImageBase))
-			printf("[*] Import Table: 0x%x\n", (ImportTable.VirtualAddress+OptHeader.ImageBase))
-			printf("[*] Import Address Table: 0x%x\n", (ImportAddressTable.VirtualAddress+OptHeader.ImageBase))
+			ExportTable = &OptHeader->DataDirectory[0];
+			ImportTable = &OptHeader->DataDirectory[1];
+			ImportAddressTable = &OptHeader->DataDirectory[12];
 
 
-			cout << "[-------------------------------------]\n\n\n"
+			printf("[*] Export Table: 0x%x\n", (ExportTable->VirtualAddress+OptHeader->ImageBase));
+			printf("[*] Import Table: 0x%x\n", (ImportTable->VirtualAddress+OptHeader->ImageBase));
+			printf("[*] Import Address Table: 0x%x\n", (ImportAddressTable->VirtualAddress+OptHeader->ImageBase));
+
+
+			cout << "[-------------------------------------]\n\n\n";
 
 
 			for (int i = 0; i < NtHeader->FileHeader.NumberOfSections; i++){
 				SectionHeader = PIMAGE_SECTION_HEADER(DWORD(PE) + DOSHeader->e_lfanew + 248 + (i * 40));
 				cout << "	##########################################\n";
 				cout << "	#                                        #\n";
-				cout << "	#   ." << SectionHeader.Name << " -> ";
+				cout << "	#   ." << SectionHeader->Name << " -> ";
 				printf("0x%x\n", SectionHeader->VirtualAddress);
 				cout << "               #\n";
 				
-				for(int j = 0; j < (SectionHeader->SizeOfRawData/(OptHeader->SizeOfImage/20); j++){
+				for(int j = 0; j < (SectionHeader->SizeOfRawData/(OptHeader->SizeOfImage/20)); j++){
 					cout << "	# 										 #\n";
 				}
 			}
@@ -98,7 +104,7 @@ void PrintInfo(char * PE){
 		}	
 	}
 	else{
-		cout << "[-] Magic number not valid !"
+		cout << "[-] Magic number not valid !";
 		cout << "[-] File is not a valid PE :(\n";
 		exit(1);
 	}
@@ -127,15 +133,15 @@ void Dump(char * PE){
 
 	DOSHeader = PIMAGE_DOS_HEADER(PE); // Initialize Variable
 	NtHeader = PIMAGE_NT_HEADERS(DWORD(PE) + DOSHeader->e_lfanew); // Initialize	
-	FileHeader = &NtHeader.FileHeader;
-	OptHeader = &NtHeader.OptionalHeader;
+	FileHeader = &NtHeader->FileHeader;
+	OptHeader = &NtHeader->OptionalHeader;
 
 
 
 	DWORD ImageBase = OptHeader->ImageBase;
 	
 	fstream File;
-	File.open ("MemDump", std::fstream::in | std::fstream::out | std::fstream::app | std::fstream::binary);
+	File.open ("MappedImage", std::fstream::in | std::fstream::out | std::fstream::app | std::fstream::binary);
 	if(File.is_open()){
 
 		cout << "[>] Maping PE headers...\n";
@@ -164,7 +170,7 @@ void Dump(char * PE){
 			printf("[>] 0x%x\n", ImageBase);
 
 			File.write((char*)(DWORD(PE) + SectionHeader->PointerToRawData), SectionHeader->SizeOfRawData);
-			Needle += SectionHeader->SizeOfRawData;
+			ImageBase += SectionHeader->SizeOfRawData;
 		}
 		File.close();
 
